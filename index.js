f3cef02f9572b8750ff1e90ea6cb707bb3979283
HarshithaKonduru214 <harshitakonduru2244@gmail.com>
2021-08-16 17:40:39 +0530

    gif updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const birthDate = document.querySelector('#date');
 const luckyNumber = document.querySelector('#lucky-number');
 const checkButton = document.querySelector('#check-button');
 const result = document.querySelector('.result');
+const image = document.querySelector('#image');
 
 checkButton.addEventListener("click", checkBirthdateLucky)
 
@@ -11,8 +12,10 @@ function checkBirthdateLucky() {
     if(dob&&luckyNumber.value) {
         if(sum % luckyNumber.value === 0) {
             result.innerText = 'Your Birthdate Is Lucky!'
+            image.src = "hooray.gif"
         }else {
             result.innerText = 'Your Birthdate Is Not Lucky!'
+            image.src = "sorry.gif"
         }
     }else {
         result.innerText = 'Please enter both the fields!'
